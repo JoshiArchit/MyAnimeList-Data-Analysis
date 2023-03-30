6a46c838141aca2c465ad97b8b61bdc04fda8e23
architJoshi2904 <aj6082@rit.edu>
2023-03-30 10:50:19 -0400

    Interesting queries - finalized

--- interestingQueries.sql
+++ interestingQueries.sql
@@ -1,58 +1,72 @@
-------------------------------------------------------------
--- CSCI-620: INTRO TO BIG DATA
--- PROJECT PHASE 2
--- FILENAME: interestingQueries.sql
--- AUTHORS: ATHINA STEWART  as1986
---          ARCHIT JOSHI    aj6082
---          PARIJAT KAWALE  pk7145
---          CHENGZI CAO     cc3773
-------------------------------------------------------------
+/*
+    CSCI-620: INTRO TO BIG DATA
+    PROJECT PHASE 2
+    FILENAME: interestingQueries.sql
+    AUTHORS:    ATHINA STEWART  as1986
+                ARCHIT JOSHI    aj6082
+                PARIJAT KAWALE  pk7145
+                CHENGZI CAO     cc3773
+
+    This file contains a few interesting SQL queries over the data set.
+*/
 
 SET search_path TO anime;
 
--- anime of genre Drama produced by Animax
+/*
+ How many drama animes has Animax produced ?
+ */
 SELECT a.anime_id, g.genre, p.producer
 FROM anime a
-INNER JOIN has_genre hg on a.anime_id = hg.anime_id
-INNER JOIN genre g on hg.genre_id = g.genre_id
-INNER JOIN produced_by pb on a.anime_id = pb.anime_id
-INNER JOIN producer p on p.producer_id = pb.producer_id
+    INNER JOIN has_genre hg ON a.anime_id = hg.anime_id
+    INNER JOIN genre g ON hg.genre_id = g.genre_id
+    INNER JOIN produced_by pb ON a.anime_id = pb.anime_id
+    INNER JOIN producer p ON p.producer_id = pb.producer_id
 AND p.producer = 'Animax'
 AND g.genre = 'Drama';
 
--- users that watch comedy anime from India
+
+/*
+ Which users have been watching comedy genre animes in India
+ */
 SELECT DISTINCT (u.user_id, u.user_location, g.genre)
 FROM users u
-INNER JOIN watches w on u.user_id = w.user_id
-INNER JOIN has_genre hg on w.anime_id = hg.anime_id
-INNER JOIN genre g on g.genre_id = hg.genre_id
-WHERE u.user_location like '%India'
+    INNER JOIN watches w ON u.user_id = w.user_id
+    INNER JOIN has_genre hg ON w.anime_id = hg.anime_id
+    INNER JOIN genre g ON g.genre_id = hg.genre_id
+WHERE u.user_location LIKE '%India'
 AND genre = 'Comedy';
-
--- who licensed the most anime's between post 2000s
 
 
 /*
-count of 9+ rated anime's produced by a studio
+    Which licensor licensed the most anime's between post 2000s
  */
-select studio.studio, count(anime.score)
-from created_by inner join studio on created_by.studio_id = studio.studio_id
-                inner join anime on created_by.anime_id = anime.anime_id
-where score > 9.0 group by studio.studio order by count(anime.score) DESC;
+SELECT licensor.licensor_id AS ID, licensor.licensor, count(licensed_by.anime_id)
+FROM licensor
+    INNER JOIN licensed_by ON licensor.licensor_id = licensed_by.licensor_id
+    INNER JOIN anime ON licensed_by.anime_id = anime.anime_id
+WHERE cast(substr(aired_from_to,11,4) AS INT) > 2000 AND aired_from_to NOT LIKE '%None%'
+GROUP BY licensor.licensor_id, licensor.licensor ORDER BY count(licensed_by.anime_id) DESC ;
+
+
+/*
+    A count of 9+ rated anime's produced by a studio
+ */
+SELECT studio.studio, count(anime.score)
+FROM created_by INNER JOIN studio ON created_by.studio_id = studio.studio_id
+                INNER JOIN anime ON created_by.anime_id = anime.anime_id
+WHERE score > 9.0 GROUP BY studio.studio ORDER BY count(anime.score) DESC;
+
 
 /*
  Ranking Genres by popularity across all anime's in a specific country
 
- Admiral Brown : Hey, Im a famous producer. I want to make a new anime for my target audience
- in China ! What genre is most popular there ?
+ "Hey, Im a famous producer. I want to make a new anime for my target audience
+ in China ! What genre is most popular there ?"
  */
-select genre, sum(popularity)
-from genre inner join has_genre on genre.genre_id = has_genre.genre_id
-           inner join anime on has_genre.anime_id = anime.anime_id
-           inner join watches on watches.anime_id = anime.anime_id
-           inner join users on watches.user_id = users.user_id
-where users.user_location like '%China%'
-group by genre order by sum(popularity) DESC;
-
-select producer
-select cast(substr(aired_from_to,11,4) as int) from anime where aired_from_to not like '%None%';
+SELECT genre, sum(popularity)
+FROM genre INNER JOIN has_genre ON genre.genre_id = has_genre.genre_id
+           INNER JOIN anime ON has_genre.anime_id = anime.anime_id
+           INNER JOIN watches  ON watches.anime_id = anime.anime_id
+           INNER JOIN users ON watches.user_id = users.user_id
+WHERE users.user_location LIKE '%China%'
+GROUP BY genre ORDER BY sum(popularity) DESC;
